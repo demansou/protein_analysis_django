415b701cab6e732e9a45dde0d65ae7eb5f80a277
Daniel Mansour <demansou@outlook.com>
2017-12-10 23:42:31 -0600

    [BUGFIX] typo in url parameter for process all queries

--- protein_analysis_tool/urls.py
+++ protein_analysis_tool/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^select_motifs/$', views.select_motifs_view, name='select-motifs'),
     url(r'^define_parameters/$', views.define_parameters_view, name='define-parameters'),
     url(r'^process_query/$', views.process_query_view, name='process-query'),
-    url(r'^process_all_queries/$', views.process_all_queries_view, name='process-all_queries'),
+    url(r'^process_all_queries/$', views.process_all_queries_view, name='process-all-queries'),
     url(r'^all_queries/$', views.all_queries_view, name='all-queries'),
 ]
