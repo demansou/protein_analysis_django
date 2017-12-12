ced62791ec8c5b1a73f313bc76237f64f28baf25
Daniel Mansour <demansou@outlook.com>
2017-12-11 23:28:20 -0600

    [PARTIAL FEATURE] updated url processing

--- protein_analysis_tool/urls.py
+++ protein_analysis_tool/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = 'protein_analysis_tool'
+
+"""
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^select_collections/$', views.select_collections_view, name='select-collections'),
@@ -12,4 +15,16 @@ urlpatterns = [
     url(r'^process_all_queries/$', views.process_all_queries_view, name='process-all-queries'),
     url(r'^all_queries/$', views.all_queries_view, name='all-queries'),
     url(r'^results/<int:result_id>/$', views.view_query_result, name='view-single-result'),
+]
+"""
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('select_collections/', views.select_collections_view, name='select-collections'),
+    path('select_motifs/', views.select_motifs_view, name='select-motifs'),
+    path('define_parameters/', views.define_parameters_view, name='define-parameters'),
+    path('process_query/', views.process_query_view, name='process-query'),
+    path('process_all_queries/', views.process_all_queries_view, name='process-all-queries'),
+    path('all_queries', views.all_queries_view, name='all_queries'),
+    path('results/<int:result_id>/', views.view_query_result, name='view-single-result'),
 ]
