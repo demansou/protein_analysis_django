a521f6913c4c83bd3382065ca44dba59d32ec15d
Daniel Mansour <demansou@outlook.com>
2017-12-13 22:49:01 -0600

    [BUGFIX] fixed all_queries/ url

--- protein_analysis_tool/urls.py
+++ protein_analysis_tool/urls.py
@@ -5,19 +5,6 @@ from . import views
 
 app_name = 'protein_analysis_tool'
 
-"""
-urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^select_collections/$', views.select_collections_view, name='select-collections'),
-    url(r'^select_motifs/$', views.select_motifs_view, name='select-motifs'),
-    url(r'^define_parameters/$', views.define_parameters_view, name='define-parameters'),
-    url(r'^process_query/$', views.process_query_view, name='process-query'),
-    url(r'^process_all_queries/$', views.process_all_queries_view, name='process-all-queries'),
-    url(r'^all_queries/$', views.all_queries_view, name='all-queries'),
-    url(r'^results/<int:result_id>/$', views.view_query_result, name='view-single-result'),
-]
-"""
-
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('select_collections/', views.select_collections_view, name='select-collections'),
@@ -25,6 +12,6 @@ urlpatterns = [
     path('define_parameters/', views.define_parameters_view, name='define-parameters'),
     path('process_query/', views.process_query_view, name='process-query'),
     path('process_all_queries/', views.process_all_queries_view, name='process-all-queries'),
-    path('all_queries', views.all_queries_view, name='all-queries'),
+    path('all_queries/', views.all_queries_view, name='all-queries'),
     path('results/<int:result_id>/', views.view_query_result, name='view-single-result'),
 ]
