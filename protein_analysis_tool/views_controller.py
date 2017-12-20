f99170ca3cd7daba2a8c55ebf072a600252d6325
Daniel Mansour <demansou@outlook.com>
2017-12-19 23:23:41 -0600

    [CHANGES] index view changed to form

--- protein_analysis_tool/views_controller.py
+++ protein_analysis_tool/views_controller.py
@@ -215,7 +215,7 @@ def index_form_view_controller(request):
     :return:
     """
 
-    collection_list = get_list_or_404(Collection)
+    collection_list = get_list_or_404(Collection, sequence_count__gte=1)
     motif_list = get_list_or_404(Motif)
 
     context = {
