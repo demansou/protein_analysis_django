b569a54652c21e5869616553870539f04845e201
Daniel Mansour <demansou@gmail.com>
2017-12-21 19:26:35 -0600

    [FORM UPDATE] Updated form to class controller

--- protein_analysis_tool/views_controller.py
+++ protein_analysis_tool/views_controller.py
@@ -66,6 +66,28 @@ def process_result_dict(result_dict):
     return result_dict
 
 
+def reset_session_error_message(request):
+    """
+
+    :param request:
+    :return:
+    """
+    try:
+        del request.session['form_error']
+    except KeyError:
+        pass
+
+
+def update_session_error_message(request, message):
+    """
+
+    :param request:
+    :param message:
+    :return:
+    """
+    request.session['form_error'] = message
+    return request
+
 #################
 # POST REQUESTS #
 #################
@@ -99,43 +121,233 @@ def process_all_queries():
     return HttpResponseRedirect('/all_queries/')
 
 
-class IndexForm(object):
+class IndexFormController(object):
     def __init__(self, request):
-        self.sequence_data_title = get_form_data_from_http_post(request, 'sequence_data_title')
-        self.sequence_data = get_form_data_from_http_post(request, 'sequence_data')
-        self.selected_collections = get_form_data_from_http_post_as_list(request, 'selected_collections[]')
-        self.selected_motifs = get_form_data_from_http_post_as_list(request, 'selected_motifs[]')
-        self.min_num_motifs = get_form_data_from_http_post(request, 'min_num_motifs')
-        self.max_motif_range = get_form_data_from_http_post(request, 'max_motif_range')
-
-        self.check_form_data(request)
-
-    def check_form_data(self, request):
         """
-
+        Initialize IndexForm controller with HTTP request.
+        Perform form analysis.
         :param request:
+        """
+        self.request = request
+
+        if self.request.POST:
+            self.sequence_data_title = get_form_data_from_http_post(self.request, 'sequence_data_title')
+            self.sequence_data = get_form_data_from_http_post(self.request, 'sequence_data')
+            self.selected_collections = get_form_data_from_http_post_as_list(self.request, 'selected_collections[]')
+            self.selected_motifs = get_form_data_from_http_post_as_list(self.request, 'selected_motifs[]')
+            self.min_num_motifs = get_form_data_from_http_post(self.request, 'min_num_motifs')
+            self.max_motif_range = get_form_data_from_http_post(self.request, 'max_motif_range')
+
+            self.process_form()
+
+        self.generate_form()
+
+    def generate_form(self):
+        """
+        Generate form
         :return:
         """
-        try:
-            del request.session['form_error']
-        except KeyError:
-            pass
+        collection_list = get_list_or_404(Collection, sequence_count__gte=1)
+        motif_list = get_list_or_404(Motif)
+
+        context = {
+            'collection_list': collection_list,
+            'motif_list': motif_list,
+        }
+
+        return render(self.request, 'protein_analysis_tool/form_index.html', context=context)
+
+    def process_form(self):
+        """
+        Process form data
+        :return:
+        """
+        # validate form fields
+        self.check_form_data()
+
+        # get selected collections (or empty list)
+        selected_collections_objects = self.check_selected_collections()
+
+        # get new collection from copy & pasted textarea data
+        selected_collections_objects = self.check_copy_paste_data(selected_collections_objects)
+
+        # get selected motifs (list cannot be empty)
+        selected_motifs_objects = self.check_selected_motifs()
+
+        # create QueryMotif database entries with data from form
+        self.create_query_motifs(selected_collections_objects, selected_motifs_objects)
+
+        return HttpResponseRedirect('/all_queries/')
+
+    def check_form_data(self):
+        """
+        Handles form errors.
+        :return:
+        """
+        reset_session_error_message(self.request)
 
         if len(self.sequence_data) == 0 and len(self.selected_collections) == 0:
-            request.session['form_error'] = 'No sequence data selected AND no collections selected'
+            err = 'No sequence data selected AND no collections selected'
+            self.request = update_session_error_message(self.request, err)
             return HttpResponseRedirect('/')
 
         if len(self.selected_motifs) == 0:
-            request.session['form_error'] = 'No motifs selected'
+            err = 'No motifs selected'
+            self.request = update_session_error_message(self.request, err)
             return HttpResponseRedirect('/')
 
         if int(self.min_num_motifs) < 2:
-            request.session['form_error'] = 'Minimum number of motifs outside of allowed range'
+            err = 'Minimum number of motifs outside of allowed range'
+            self.request = update_session_error_message(self.request, err)
             return HttpResponseRedirect('/')
 
         if int(self.max_motif_range) < 20 or int(self.max_motif_range) > 200:
-            request.session['form_error'] = 'Maximum motif range outside of allowed range'
+            err = 'Maximum motif range outside of allowed range'
+            self.request = update_session_error_message(self.request, err)
             return HttpResponseRedirect('/')
+
+    def check_selected_collections(self):
+        """
+        Checks for selected collections in form. Returns list of objects or empty list.
+        :return:
+        """
+        # if selected_collections is false (from import) return empty list
+        if not self.selected_collections:
+            return []
+
+        return [Collection.objects.get(pk=int(c) for c in self.selected_collections)]
+
+    def check_copy_paste_data(self, selected_collections_objects):
+        """
+        Checks for textarea input. Attempts to create new database entry with new collection data.
+        :return:
+        """
+        # clear session error message if one present
+        reset_session_error_message(self.request)
+
+        # return only selected collections if no data input into textarea
+        if len(str(self.sequence_data)) == 0:
+            return selected_collections_objects
+
+        # attempt to create new record or update previous record
+        new_collection = Collection.objects.update_or_create(
+            collection_name=str(self.sequence_data_title),
+            pub_date=timezone.now(),
+            collection_parsed=False,
+            sequence_count=0
+        )
+
+        # create file object with textarea data
+        content = ContentFile(str(self.sequence_data))
+
+        # create new file with randomized name and save textarea data to field
+        new_collection.collection_file.save(
+            self.clean_path_from_filename(self.create_temp_filename()), content, save=True)
+
+        # md5 hash saved file
+        new_collection.collection_hash = large_file_hasher(new_collection.collection_file.path)
+
+        # save file and hash to database
+        new_collection.save()
+
+        self.parse_new_collection_file_data(new_collection)
+
+        if not new_collection.pk:
+            err = 'Failed to add new data to database. Cannot parse new collection data from text input'
+            self.request = update_session_error_message(self.request, err)
+            return HttpResponseRedirect('/')
+
+        # return list of selected collections with new collection added
+        return selected_collections_objects.append(new_collection)
+
+    def parse_new_collection_file_data(self, new_collection):
+        """
+
+        :param new_collection:
+        :return:
+        """
+        # clear session error message if one present
+        reset_session_error_message(self.request)
+
+        # iterate through fasta file and update record if exists with current collection as FK or create otherwise
+        for record in SeqIO.parse(new_collection.collection_file.path, 'fasta'):
+            Sequence.objects.update_or_create(
+                collection_fk=new_collection,
+                sequence_id=record.id,
+                sequence_name=str(record.name),
+                sequence=str(record.seq)
+            )
+
+        # set collection to parsed
+        new_collection.collection_parsed = True
+
+        # get count of sequences related to collection after update
+        new_collection.sequence_count = Sequence.objects.filter(collection_fk=new_collection).count()
+
+        # save changes to collection
+        new_collection.save()
+
+        if not Collection.objects.get(pk=new_collection.pk).collection_parsed:
+            err = 'Error: Collection not parsed.'
+            self.request = update_session_error_message(self.request, err)
+            return HttpResponseRedirect('/')
+
+    def check_selected_motifs(self):
+        """
+        Checks for selected motifs in form. Returns list of objects.
+        Cannot be empty because form requires at least one motif to process form.
+        :return:
+        """
+        return [Motif.objects.get(pk=int(m) for m in self.selected_motifs)]
+
+    def create_query_motifs(self, selected_collections_objects, selected_motifs_objects):
+        """
+        Create Query database entry for each collection/motif pair.
+        :param selected_collections_objects:
+        :param selected_motifs_objects:
+        :return:
+        """
+        # clear session error message if one present
+        reset_session_error_message(self.request)
+
+        if len(selected_collections_objects) == 0 or len(selected_motifs_objects) == 0:
+            err = 'Error occurred creating queries from data. # Collections: {0}, # Motifs: {1}'.format(
+                len(selected_collections_objects),
+                len(selected_motifs_objects)
+            )
+            update_session_error_message(self.request, err)
+            return HttpResponseRedirect('/')
+
+        for collection in selected_collections_objects:
+            for motif in selected_motifs_objects:
+                Query.objects.update_or_create(
+                    collection_fk=collection,
+                    motif_fk=motif,
+                    min_num_motifs_per_sequence=int(self.min_num_motifs),
+                    max_char_distance_between_motifs=int(self.max_motif_range)
+                )
+
+    @staticmethod
+    def create_temp_filename():
+        """
+        Returns file name with full path.
+        :return:
+        """
+        return tempfile.NamedTemporaryFile(
+            suffix='.fasta',
+            prefix='',
+            dir=MEDIA_ROOT,
+            delete=False
+        ).name
+
+    @staticmethod
+    def clean_path_from_filename(file_name):
+        """
+        Returns file name from full path.
+        :param file_name:
+        :return:
+        """
+        return os.path.basename(file_name)
 
 
 def index_form_process_controller(request):
